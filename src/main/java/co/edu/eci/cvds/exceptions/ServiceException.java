42e89245bb7e6f2b330dc41bb1a7b3a62677a3a1
Samuel Rojas <samu.02309@gmail.com>
2024-05-02 22:06:04 -0500

    Exceptions

---
+++ src/main/java/co/edu/eci/cvds/exceptions/ServiceException.java
@@ -0,0 +1,11 @@
+package co.edu.eci.cvds.exceptions;
+
+public class ServiceException extends Exception{
+
+    // public final static String  = "";
+
+    public ServiceException(String message){
+        super(message);
+    }
+
+}
