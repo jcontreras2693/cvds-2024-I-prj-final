832e19b16daaf452b0c08990270bb35709668133
johanestrada01 <johan.estrada-p@mail.escuelaing.edu.co>
2024-05-06 19:18:16 -0500

    avance

--- src/main/java/co/edu/eci/cvds/controller/CategoryController.java
+++ src/main/java/co/edu/eci/cvds/controller/CategoryController.java
@@ -1,4 +1,46 @@
 package co.edu.eci.cvds.controller;
 
+
+import co.edu.eci.cvds.model.Category;
+import co.edu.eci.cvds.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.lang.annotation.Retention;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/category")
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService){
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/category/getCategories")
+    @ResponseBody
+    public List<Category> getCategories(Model model){
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/category/putCategory")
+    @ResponseBody
+    public List<Category> addCategory(@RequestBody Category category){
+        categoryService.addCategory(category);
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/category/getCategory/{id}")
+    
+
+
 }
