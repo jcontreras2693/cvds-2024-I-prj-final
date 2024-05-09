74b5c26189908488451bb74a28dd4c66e3870e9c
anndr0 <ana.duran-b@mail.escuelaing.edu.co>
2024-05-09 18:58:48 -0500

    pagina sin divs en carrito de compras

--- src/main/resources/static/categoryCode.js
+++ src/main/resources/static/categoryCode.js
@@ -282,3 +282,22 @@ function containsIdYear2020(id) {
     }
     return false;
 }
+
+
+
+// boton del carrito, el contenido del carrito y el boton de cerrar
+const cartButton = document.querySelector('.cart');
+const cartContent = document.querySelector('.cart-content');
+const closeButton = document.querySelector('#close-button');
+
+cartButton.addEventListener('click', function() {
+    cartContent.classList.toggle('show');
+});
+
+closeButton.addEventListener('click', function() {
+    cartContent.classList.add('hide');
+    setTimeout(() => {
+        cartContent.classList.remove('show');
+    }, 500);
+});
+
