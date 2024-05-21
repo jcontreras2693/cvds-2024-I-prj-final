0315ee22c0c249e772de01c8e9db02a366c2f56f
anndr0 <ana.duran-b@mail.escuelaing.edu.co>
2024-05-21 15:17:30 -0500

    imagenes segun servicio

--- src/main/resources/static/js/categoryCode.js
+++ src/main/resources/static/js/categoryCode.js
@@ -28,3 +28,21 @@ closeButton.addEventListener('click', function() {
     cartContent.classList.add('hide');
     isCartOpen = false;
 });
+
+
+// Función para formatear el texto del nombre del servicio
+function formatServiceName(name) {
+    // Convertir a minúsculas y reemplazar espacios con guiones
+    return name.toLowerCase().replace(/\s+/g, '-').normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+// Obtener todos los elementos de imagen y actualizar sus rutas
+document.querySelectorAll('[id^="img-"]').forEach(function(img) {
+    var serviceName = img.getAttribute('id').substring(4); // Obtener el nombre del servicio
+    var formattedServiceName = formatServiceName(serviceName); // Formatear el nombre del servicio
+    var categoryName = img.getAttribute('data-category'); // Obtener el nombre de la categoría desde el atributo data-
+    var formatCategoryName= formatServiceName(categoryName);
+    img.setAttribute('src', '/static/images/servicios/' + formattedServiceName + '.png'); // Actualizar la ruta de la imagen
+});
+
+
